69728eeef1d934dc4d8b423acb4ca4124bee0dea
harperhe1 <harperhe@hotmail.com>
2022-11-15 17:16:30 -0500

    Module 3 fixed again

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,9 +9,7 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <li>
-                            (this.props.quiz_question.answer_options[0])
-                        </li>
+                        <li>(this.props.quiz_question.answer_options[0])</li>
                     </ul>
                 </section>
             </main>              
