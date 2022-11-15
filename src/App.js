42b891494ba8e192759ead30221b1acad1e32185
harperhe1 <harperhe@hotmail.com>
2022-11-15 15:54:59 -0500

    Section 2 commitments

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import './App.css'
 import Quiz from './Quiz.js'
 
 class App extends Component {
-  render() {
-    return (
-        <Quiz />
-    )
-  }
+    render() {
+        return (
+            <Quiz />
+        )
+    }
 }
-
 export default App
