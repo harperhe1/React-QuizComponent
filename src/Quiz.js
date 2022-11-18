266a4aa65edcc8e2ac7089399d55668dd185f365
harperhe1 <harperhe@hotmail.com>
2022-11-18 17:36:01 -0500

    Changes to Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,10 +10,13 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     render() {
-        const isQuizEnd 
+        const isQuizEnd = quizData.quiz_questions.length
         return (
             <div>
-                <QuizEnd />
+                if(isQuizEnd)
+                {
+                    <QuizEnd />
+                }                
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         )
