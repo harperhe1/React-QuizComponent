1324ed2d4498a7df89b8955f8320215843c9ff9f
harperhe1 <harperhe@hotmail.com>
2023-01-18 17:09:32 -0500

    Module 7 Fixed

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -29,7 +29,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
-                this.state.incorrectAnswer: className='error' ? null
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main>              
         )
     }
