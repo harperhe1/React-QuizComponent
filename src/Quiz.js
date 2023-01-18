11a96d75b1385cbcb43bc4f54f01248798154fcd
harperhe1 <harperhe@hotmail.com>
2023-01-18 17:33:23 -0500

    Module 8

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,9 @@ class Quiz extends Component {
             return { quiz_position: state.quiz_position + 1 }
         })
     }
+    handleResetClick() {
+        this.setState() => { return {quiz_position: 1}}
+    }
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
