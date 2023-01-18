cad03909ca636ef3663939a3cfb795b50503dcde
harperhe1 <harperhe@hotmail.com>
2023-01-18 16:57:47 -0500

    Module 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {incorrectAnswer: false}
+    }
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler()
@@ -21,6 +25,7 @@ class QuizQuestion extends Component {
                         })}
                     </ul>
                 </section>
+                this.state.incorrectAnswer: className='error' ? null
             </main>              
         )
     }
